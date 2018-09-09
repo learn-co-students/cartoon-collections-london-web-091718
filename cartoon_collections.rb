c663f25841a682362b5a80494284fe61764b3673
Sang Song <sangyoon.song@gmail.com>
2018-09-09 14:20:36 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arg)
+  arg.each_with_index{|x, i| puts "#{i+1}. #{x}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arg)
+  arg.collect {|x| x.capitalize+"!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arg)
+  if arg.any? {|x| x.length > 4}
+    return true
+  else
+    return false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arg)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arg.find{|x| cheese_types.include?(x)}
 end
